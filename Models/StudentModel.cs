6dcd0f0700baf3acc8e3f266c52eccb072127805
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-09-04 18:15:43 +0530

    P2-AssUsingCustomBinding

---
+++ Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsingCustomModelBinding.Models
+{
+    [Table("Student")]
+    public class StudentModel
+    {
+        [Key]
+        public string Name { get; set; }
+        public string WhichClass { get; set; }
+        public string Address { get; set; }
+        public int TotalMarks { get; set; }
+
+    }
+}
